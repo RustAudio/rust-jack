0a2c7df749fbc3567ca81102cd36e2405e640d27
Will S. Medrano <will.s.medrano@gmail.com>
2016-12-31 11:58:47 -0800

    threadsafe

--- src/info.rs
+++ src/info.rs
@@ -1,20 +1,23 @@
 use std::ffi;
-use std::sync::{Once, ONCE_INIT};
+use std::cell::Cell;
+use std::sync::{Mutex, Once, ONCE_INIT};
 use jack_sys as j;
 
 fn to_nothing(_: &str) {}
 
-static mut INFO_FN: fn(&str) = to_nothing;
-static mut ERROR_FN: fn(&str) = to_nothing;
+lazy_static! {
+    static ref INFO_FN: Mutex<Cell<fn(&str)>> = Mutex::new(Cell::new(to_nothing));
+    static ref ERROR_FN: Mutex<Cell<fn(&str)>> = Mutex::new(Cell::new(to_nothing));
+}
 
 unsafe extern "C" fn error_wrapper(msg: *const i8) {
     let msg = ffi::CStr::from_ptr(msg).to_str().unwrap();
-    ERROR_FN(msg);
+    ERROR_FN.lock().unwrap().get()(msg);
 }
 
 unsafe extern "C" fn info_wrapper(msg: *const i8) {
     let msg = ffi::CStr::from_ptr(msg).to_str().unwrap();
-    INFO_FN(msg)
+    INFO_FN.lock().unwrap().get()(msg)
 }
 
 static IS_INFO_CALLBACK_SET: Once = ONCE_INIT;
@@ -29,9 +32,7 @@ static IS_INFO_CALLBACK_SET: Once = ONCE_INIT;
 /// jack::set_info_callback(info_log);
 /// ```
 pub fn set_info_callback(info: fn(&str)) {
-    unsafe {
-        INFO_FN = info;
-    }
+    INFO_FN.lock().unwrap().set(info);
     IS_INFO_CALLBACK_SET.call_once(|| unsafe { j::jack_set_info_function(Some(info_wrapper)) })
 }
 
@@ -47,8 +48,6 @@ static IS_ERROR_CALLBACK_SET: Once = ONCE_INIT;
 /// jack::set_error_callback(error_log);
 /// ```
 pub fn set_error_callback(error: fn(&str)) {
-    unsafe {
-        ERROR_FN = error;
-    }
+    ERROR_FN.lock().unwrap().set(error);
     IS_ERROR_CALLBACK_SET.call_once(|| unsafe { j::jack_set_error_function(Some(error_wrapper)) })
 }
