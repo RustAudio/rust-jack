5690cd0ae3c2a15036c820f142b6190f50e6938f
Will S. Medrano <will.s.medrano@gmail.com>
2017-02-06 21:00:11 -0800

    increase sleep

--- src/client/common.rs
+++ src/client/common.rs
@@ -5,7 +5,7 @@ pub fn sleep_on_test() {
     #[cfg(test)]
     {
         use std::{thread, time};
-        thread::sleep(time::Duration::from_millis(200));
+        thread::sleep(time::Duration::from_millis(300));
     }
 }
 
