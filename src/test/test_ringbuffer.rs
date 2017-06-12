4202b7433150327cead798ca2afb2c0cd6ce68db
Yuval Kohavi <yuval.kohavi@gmail.com>
2017-06-11 21:18:44 -0400

    documentation, made get_vector public in case it is needed.

--- src/test/test_ringbuffer.rs
+++ src/test/test_ringbuffer.rs
@@ -8,40 +8,38 @@ fn ringbuffer_can_create() {
 
 #[test]
 fn ringbuffer_can_space() {
-    const SIZE : usize = 1024;
+    const SIZE: usize = 1024;
     let ringbuf = RingBuffer::new(SIZE).unwrap();
     let (mut reader, mut writer) = ringbuf.into_reader_writer();
 
-    assert_eq!(writer.space(), SIZE-1);
+    assert_eq!(writer.space(), SIZE - 1);
     assert_eq!(reader.space(), 0);
 
-    const ADVANCE : usize = 5;
+    const ADVANCE: usize = 5;
 
     writer.advance(ADVANCE);
 
-    assert_eq!(writer.space(), SIZE-1-ADVANCE);
+    assert_eq!(writer.space(), SIZE - 1 - ADVANCE);
     assert_eq!(reader.space(), ADVANCE);
 
     reader.advance(ADVANCE);
-    assert_eq!(writer.space(), SIZE-1);
+    assert_eq!(writer.space(), SIZE - 1);
     assert_eq!(reader.space(), 0);
 
-    
+
 }
-
-
 
 #[test]
 fn ringbuffer_write_read() {
     let ringbuf = RingBuffer::new(1024).unwrap();
     let (mut reader, mut writer) = ringbuf.into_reader_writer();
 
-    let buf = [0u8,1,2,3];
+    let buf = [0u8, 1, 2, 3];
     let num = writer.write_buffer(&buf);
     assert_eq!(num, buf.len());
 
 
-    let mut outbuf = [0u8;8];
+    let mut outbuf = [0u8; 8];
     let num = reader.read_buffer(&mut outbuf);
     assert_eq!(num, buf.len());
 
@@ -50,44 +48,68 @@ fn ringbuffer_write_read() {
 }
 
 #[test]
-fn ringbuffer_peak_write() {
+fn ringbuffer_peek_write() {
     let ringbuf = RingBuffer::new(1024).unwrap();
     let (reader, mut writer) = ringbuf.into_reader_writer();
 
-    let buf = [0u8,1,2,3];
+    let buf = [0u8, 1, 2, 3];
     writer.write_buffer(&buf);
 
-    let mut outbuf = [0u8;8];
+    let data: Vec<u8> = reader.peek_iter().map(|x| *x).collect();
 
-    let mut num = 0;
-    for (i, item) in reader.peek_iter().enumerate() {
-        outbuf[i] = *item;
-        num = i + 1;
-    }
- 
-    assert_eq!(num, buf.len());
-    assert_eq!(outbuf[..num], buf[..]);
+    assert_eq!(data.len(), buf.len());
+    assert_eq!(data[..], buf[..]);
 
 }
 
 #[test]
-fn ringbuffer_peak_read() {
+fn ringbuffer_write_read_split() {
+    const BUFSIZE: usize = 10;
+    let ringbuf = RingBuffer::new(BUFSIZE).unwrap();
+    let (mut reader, mut writer) = ringbuf.into_reader_writer();
+
+    let buf = [0u8, 1, 2, 3];
+
+    let advancedsize = BUFSIZE / (buf.len() / 2);
+    writer.advance(advancedsize);
+    reader.advance(advancedsize);
+    {
+        let (_, v2) = writer.get_vector();
+        assert_ne!(v2.len(), 0);
+    }
+
+    writer.write_buffer(&buf);
+
+    {
+        let (v1, _) = reader.get_vector();
+        assert_ne!(v1.len(), 0);
+    }
+
+
+    let data: Vec<u8> = reader.peek_iter().map(|x| *x).collect();
+
+    assert_eq!(data.len(), buf.len());
+    assert_eq!(data[..], buf[..]);
+
+}
+
+#[test]
+fn ringbuffer_peek_read() {
     let ringbuf = RingBuffer::new(1024).unwrap();
     let (mut reader, mut writer) = ringbuf.into_reader_writer();
 
 
-    let buf = [0u8,1,2,3];
-    for (i, item) in writer.peek_iter().take(buf.len()).enumerate() {
-        *item = buf[i];
+    let buf = [0u8, 1, 2, 3];
+    for (item, bufitem) in writer.peek_iter().zip(buf.iter()) {
+        *item = *bufitem;
     }
-  
+
     writer.advance(buf.len());
- 
-    let mut outbuf = [0u8;8];
+
+    let mut outbuf = [0u8; 8];
     let num = reader.read_buffer(&mut outbuf);
     assert_eq!(num, buf.len());
 
     assert_eq!(outbuf[..num], buf[..]);
 
 }
-
